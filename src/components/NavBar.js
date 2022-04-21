e5508859fd546283b46686cca9c85157230bb382
hardishc <contacthardish@gmail.com>
2022-04-20 18:50:49 -0600

    Setup main page

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import { Grid, Button, ButtonGroup, AppBar } from '@mui/material'
+import React from 'react'
+import { FaInstagram } from 'react-icons/fa';
+
+var option = ["Home", "Skills", "Projects", "Contact"]
+function NavBar() {
+    return (
+        <div>
+            <AppBar sx={{ height: 100, justifyContent: "center" }}>
+                <Grid container direction="row" justifyContent="center">
+                    <Grid container xs={1}>
+                        <FaInstagram size={30} />
+                    </Grid>
+                    <Grid container xs={10} justifyContent="flex-end">
+                        <ButtonGroup variant="string" aria-label="outlined primary button group">
+                            {option.map((page) => (
+                                <Button key={page}>{page}</Button>
+                            ))}
+                        </ButtonGroup>
+                    </Grid>
+                </Grid>
+            </AppBar>
+        </div >
+    )
+}
+
+export default NavBar
---
+++ src/components/SideBar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
+import './SideBar.css';
+
+function SideBar() {
+
+    return (
+        <div className='side'>
+            <ul className='nav-menu'>
+                <li>
+                   <FaInstagram size={30} />
+                </li>
+                <li>
+                    <FaGithub size={30} />
+                </li>
+                <li>
+                    <FaLinkedin size={30}/>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default SideBar
